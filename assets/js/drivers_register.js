24a984b1ed9de213f4a9319f0bfc7077936d32e5
modlenine <chainarong039@gmail.comm>
2025-03-12 16:00:10 +0700

    update register page

--- assets/js/drivers_register.js
+++ assets/js/drivers_register.js
@@ -1,5 +1,9 @@
 $(document).ready(()=>{
     $('#btn-save-driverregister').click(()=>{
+
+        //Check Value On Register form
+        
+
         $('#privacy_policy_modal').modal('show');
     });
 });
